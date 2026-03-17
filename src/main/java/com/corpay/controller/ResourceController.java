082f3fa48d135aed137cb413aa78b56cfdfca29c
Sukant Tiwari <sukant.tiwari@fleetcor.com>
2026-03-17 23:39:47 +0530

    Update ResourceController.java

--- src/main/java/com/corpay/controller/ResourceController.java
+++ src/main/java/com/corpay/controller/ResourceController.java
@@ -1,22 +1,20 @@
 package com.corpay.controller;
 
+import java.util.List;
+
+import com.corpay.dto.Menu;
+import com.corpay.service.ResourceService;
+
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import com.corpay.dao.dbo.LinkResource;
-import com.corpay.dto.Menu;
-import com.corpay.repository.ResourceRepository;
-import com.corpay.service.ResourceService;
 
 @Path("/resource")
 @Produces(MediaType.APPLICATION_JSON)
@@ -31,7 +29,7 @@ public class ResourceController {
     public Response getResources(@Context SecurityContext securityContext) {
         String emailId = securityContext.getUserPrincipal().getName();
 
-        Set<Menu> resourceSet = resourceService.findUserResources(emailId);
+        List<Menu> resourceSet = resourceService.findUserResources(emailId);
         return Response.ok(resourceSet).build();
     }
 }
